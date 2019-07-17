f6e66650ce63f23f7c64a6306f75800d5d9f65e2
Eugen Kondratiev <kondratiev.es@gmail.com>
2019-07-17 20:00:31 +0300

    getDayRaport return promise and day report table as a result

--- node-modules/date-utils.js
+++ node-modules/date-utils.js
@@ -29,9 +29,11 @@ function getNiceday(day) {
     return day > 9 ? "" + day : "0" + day;
 }
 //==============================================================================
-function dayReportSql(day, month , year) {
+function dayReportSql(day, month , year, eco = 1) {
     const startDay = formDayStr(day, month , year);
-    return `SELECT dt, w_38, q_39, T_41, T_42, P_19, P_18, T_10, P_36  FROM eco2.hr3 where dt between '${startDay}' and DATE_ADD('${startDay}', INTERVAL 23 hour)`;
+    return eco == 2 
+    ? /**change for eco2 */  `SELECT dt, w_38, q_39, T_41, T_42, P_19, P_18, T_10, P_36  FROM eco2.hr3 where dt between '${startDay}' and DATE_ADD('${startDay}', INTERVAL 23 hour)`
+    :   `SELECT dt, w_38, q_39, T_41, T_42, P_19, P_18, T_10, P_36  FROM eco2.hr3 where dt between '${startDay}' and DATE_ADD('${startDay}', INTERVAL 23 hour)`;
 }
 //========================================================================================
 function formDayStr(day = 20, month = 1, fullyear = 2019) {
